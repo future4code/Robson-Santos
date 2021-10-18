134d2ce17b8912cc2911ffe770bfbe57001d0e2a
Robson <robson.2501@hotmail.com>
2021-10-18 10:53:50 -0300

    Adicionado exercicio 13A

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -138,7 +138,13 @@ function retornaPessoaAnonimizada(pessoa) {
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+    const pessoasAutorizadas = pessoas.filter((pessoa) => {
+        if (pessoa.altura > 1.5 && pessoa.idade > 14 && pessoa.idade < 60) {
+            return pessoa
+        }
+    })
+
+    return pessoasAutorizadas
 }
 
 // EXERCÍCIO 13B
