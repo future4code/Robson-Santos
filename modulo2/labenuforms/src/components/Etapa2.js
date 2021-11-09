647a9d69569c3196c8a4ad7ec02fd412f347b62c
Robson <robson.2501@hotmail.com>
2021-11-08 23:08:36 -0300

    Página de formularios

---
+++ modulo2/labenuforms/src/components/Etapa2.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+function Etapa2() {
+    return (
+      <div>
+          <h1>ETAPA 2 - INFORMAÇÕES DO ENSINO SUPERIOR</h1>
+          <ol>Qual curso?</ol>
+          <input type="text"></input>
+          <ol>Qual a unidade de ensino?</ol>
+          <input type="text"></input>
+        
+      </div>
+    );
+  }
+  
+  export default Etapa2;
