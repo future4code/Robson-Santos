3df2592139c7575666f7a5aa07029a5a07a3e07c
Robson <robson.2501@hotmail.com>
2021-11-30 16:49:19 -0300

    subindo tela renderizada

---
+++ modulo3/useEffect/aula32-use-effect/src/components/PokeCard.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const PokeCard = ()  => {
+    const [pokemon, setPokemon] = useState({})
+
+  const pegaPokemon = pokemon => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then(response => {
+          console.log(response.pokemon)
+        setPokemon(response.pokemon)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    pegaPokemon()
+  }, [pokemon])
+
+    return (
+      <div>
+        <p>{pokemon.name}</p>
+        <p>{pokemon.weight} Kg</p>
+        {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+        {pokemon.sprites && (
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        )}
+      </div>
+    );
+  }
+
+  export default PokeCard
