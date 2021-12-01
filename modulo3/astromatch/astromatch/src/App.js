01379a71e801b226b0e9cbef1b1e34c0d98f697f
Robson <robson.2501@hotmail.com>
2021-12-01 10:28:24 -0300

    render pagina match

--- modulo3/astromatch/astromatch/src/App.js
+++ modulo3/astromatch/astromatch/src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import PaginaInicial from "./pages/PaginaInicial/PaginaInicial";
+import PaginaMatch from "./pages/PaginaMatch/PaginaMatch";
 
 function App() {
   return (
     <div>
       <PaginaInicial/>
+      <PaginaMatch />
     </div>
   );
 }
 
-export default App;
+export default App
--- modulo3/astromatch/astromatch/src/pages/PaginaMatch/PaginaMatch.js
+++ modulo3/astromatch/astromatch/src/pages/PaginaMatch/PaginaMatch.js
@@ -3,7 +3,11 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 
 const PaginaMatch = () => {
-
+    return(
+        <div>
+            Pagina de Match
+        </div>
+    )    
 }
 
 export default PaginaMatch
