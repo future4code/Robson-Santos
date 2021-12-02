bf7292e6062f5ed0c399d8cc05ada79cc045e02a
Robson <robson.2501@hotmail.com>
2021-12-02 14:06:46 -0300

    map not working

--- modulo3/astromatch/astromatch/src/pages/PaginaMatch/PaginaMatch.js
+++ modulo3/astromatch/astromatch/src/pages/PaginaMatch/PaginaMatch.js
@@ -3,22 +3,24 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 import { BASE_URL } from "../../constants/url"
 
-const PaginaMatch = () => {
+const PaginaMatch = (props) => {
     const [perfilCombinado, setPerfilCombinado] = useState([])
 
     const mostrarMatches = () => {
         axios.get(`${BASE_URL}/matches`)
         .then((response) => {
+            console.log(response.data.matches)
             setPerfilCombinado(response.data.matches)
         }).catch((error) => {
-            console.log(error.data)
+            console.log(error)
         })
     }
 
-    const limparMatches = () => {
+    const limparMatches = (props) => {
         axios.put(`${BASE_URL}/clear`)
         .then((response) => {
             console.log(response.data)
+            props.mostrarPerfil()
         }).catch((error) => {
             console.log(error.data)
         })
@@ -26,13 +28,11 @@ const PaginaMatch = () => {
 
         useEffect(() => {
         mostrarMatches()
-    }, [])
-
- 
+    }, []) 
 
     const mapearMatches = () => {
         perfilCombinado.map((pessoa) => {
-            return <div>
+            return <div key={pessoa.id}>
                 {pessoa.name}
             </div>
         })
@@ -41,7 +41,8 @@ const PaginaMatch = () => {
     return (
         <div>
             Página Match
-            
+            {mapearMatches}
+            <button onClick={limparMatches}>Limpar</button>
         </div>
     )
 }
