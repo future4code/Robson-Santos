64f53d3d5807cc29901ba36a45f3891cd88481be
Robson <robson.2501@hotmail.com>
2021-12-06 20:11:07 -0300

    pages e componentes criados

---
+++ modulo3/projeto-labex/labex/src/pages/CreateTripPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateTripPage = () => {
+
+}
+
+export default CreateTripPage
---
+++ modulo3/projeto-labex/labex/src/pages/TripDetailsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TripDetailsPage = () => {
+
+}
+
+export default TripDetailsPage
