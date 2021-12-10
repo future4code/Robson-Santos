f196c0811754d75aa6272dbf454f9ebedb4891cf
Robson <robson.2501@hotmail.com>
2021-12-09 21:27:42 -0300

    styled Cars de viagens

--- modulo3/projeto-labex/labex/src/pages/ListTripsPage.js
+++ modulo3/projeto-labex/labex/src/pages/ListTripsPage.js
@@ -8,18 +8,32 @@ import styled from "styled-components"
 
 //styled
 const ContainerPrincipal = styled.div`
-    display: flex;
-    flex-direction: column;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
     background-color: white;
-    margin: 6vw 4vh 5px;
+    margin: 6vh 15vw 5px;
     padding: 15px;
     border: 2px solid black;
+
+    
+    @media(max-width: 815px) {
+        display: flex;
+        flex-direction: column;
+    }
 `
 
 const ContainerCard = styled.div`
     display: flex;
+    flex-direction: column;
     margin: 1vh;
-    margin: 2px solid black;
+    border: 2px solid black;
+    border-radius: 20px 5px;
+    padding: 1vh;
+`
+
+const Titulo = styled.h3`
+    display: flex;
+    justify-content: center;
 `
 
 const ListTripsPage = () => {
@@ -52,7 +66,7 @@ const ListTripsPage = () => {
     //map
     const showTrips = trip.map((trip) => {
         return <ContainerCard key={trip.id}>
-            <p>{trip.name}</p> <p>{trip.description}</p> <p>{trip.planet}</p> <p>{trip.durationInDays}</p> <p>{trip.date}</p>
+            <Titulo>{trip.name}</Titulo> <p>{trip.description}</p> <p>{trip.planet}</p> <p>{trip.durationInDays}</p> <p>{trip.date}</p>
         </ContainerCard>
     })
 
