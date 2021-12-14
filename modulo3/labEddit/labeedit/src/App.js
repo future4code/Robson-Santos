6fed02101c3e58db19b85bfca7675ceabdb3313d
Robson <robson.2501@hotmail.com>
2021-12-13 21:27:02 -0300

    Bulma adicionado

--- modulo3/labEddit/labeedit/src/App.js
+++ modulo3/labEddit/labeedit/src/App.js
@@ -1,10 +1,22 @@
 import React from "react"
+import 'bulma/css/bulma.min.css'
 
 const App = () => {
   return (
-    <div>
-      Teste
-    </div>
+    <div classname="main"> 
+          <div className="buttons">
+            <button class="button is-primary">Primary</button>
+            <button class="button is-link">Link</button>
+            <button class="button is-info">Info</button>
+            <button class="button is-success">Success</button>
+            <button class="button is-warning">Warning</button>
+            <button class="button is-danger">Danger</button>
+            <button class="button is-black">Black</button>
+            <button class="button is-white">White</button>
+            <button class="button is-dark">Dark</button>
+            <button class="button is-light">Light</button>
+          </div>
+        </div>
   )
 }
 
