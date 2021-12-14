07230f098bcd7572bbb6896cb57afaea9dfcff61
Robson <robson.2501@hotmail.com>
2021-12-14 13:47:49 -0300

    Página de login

--- modulo3/labEddit/labeedit/src/App.js
+++ modulo3/labEddit/labeedit/src/App.js
@@ -1,45 +1,11 @@
 import React from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-import HomePage from './pages/HomePage'
-import Login from './pages/Login'
-import CreateAccount from './pages/CreateAccount'
-import Feed from './pages/Feed'
-import Post from './pages/Post'
-import Error from './pages/Error'
 import { ChakraProvider } from '@chakra-ui/react'
+import RouteApp from "./route/RouteApp"
 
 const App = () => {
   return (
     <ChakraProvider>
-      <BrowserRouter>
-        <Switch>
-
-          <Route exact path={'/'}>
-            <HomePage/>
-          </Route>
-
-          <Route exact path={'/login'}>
-            <Login/>
-          </Route>
-
-          <Route exact path={'/register'}>
-            <CreateAccount/>
-          </Route>
-
-          <Route exact path={'/feed'}>
-            <Feed/>
-          </Route>
-
-          <Route exact path={'/post'}>
-            <Post/>
-          </Route>
-
-          <Route>
-            <Error/>
-          </Route>
-
-        </Switch>      
-      </BrowserRouter>
+      <RouteApp/>
     </ChakraProvider>
   )
 }
