ac43f215e2da1794a535c49db836b479ba4fb5be
Robson <robson.2501@hotmail.com>
2021-12-14 21:46:13 -0300

    Página de cadastro

--- modulo3/labEddit/labeedit/src/pages/CreateAccount.js
+++ modulo3/labEddit/labeedit/src/pages/CreateAccount.js
@@ -1,9 +1,74 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { baseURL } from "../constants/url"
+import { useHistory } from "react-router-dom"
 
 const CreateAccount = () => {
+    //history
+    const history = useHistory()
+    const goToHome = () => {
+        history.push("/")
+    }
+
+    //variáveis
+    const [form, setForm] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+    const body = {
+        username: form.username,
+        email: form.email,
+        password: form.password
+    }
+
+    //funções
+    const sendRegisterDetails = (event) => {
+        event.preventDefault()
+        const {name, value} = event.target
+        setForm({
+            ...form,
+            [name]: value
+        })
+    }
+
+    const signup = () => {
+        axios.post(`${baseURL}/users/signup`, body)
+        .then((response) => {
+            localStorage.setItem('token', response.data.token)
+            history.push("/feed")
+        })
+        .catch((error) => {
+            console.log(error.response)
+        })
+    }
+
     return (
         <div>
-            Criar conta
+            <div>
+                <form onSubmit={signup}>
+                    <input 
+                        value = {form.username}
+                        name = 'username'
+                        onChange = {sendRegisterDetails}
+                        required                    
+                    />
+                    <input 
+                        value = {form.email}
+                        name = 'email'
+                        onChange = {sendRegisterDetails}
+                        required                    
+                    />
+                    <input 
+                        value = {form.password}
+                        name = 'password'
+                        onChange = {sendRegisterDetails}
+                        required                    
+                    />
+                        <button> Enviar </button>        
+                </form>
+            </div>
+            <button onClick={goToHome}> Voltar </button>
         </div>
     )
 }
--- modulo3/labEddit/labeedit/src/pages/Login.js
+++ modulo3/labEddit/labeedit/src/pages/Login.js
@@ -1,21 +1,10 @@
 import React from "react"
-import { Container, FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, Button, ButtonGroup } from '@chakra-ui/react'
 
 const Login = () => {
     return (
-        <Container>
-            <FormControl id='email' isRequired>
-                <FormLabel>E-mail</FormLabel>
-                <Input type='email' />
-            </FormControl>
-            <FormControl id='password' isRequired>
-                <FormLabel>Senha</FormLabel>
-                <Input type='password' />
-            </FormControl>
-                <Button colorScheme='teal' size='md' variant='ghost'>
-                    Enviar
-                </Button>           
-        </Container>
+        <div>
+            Login
+        </div>
     )
 }
 
