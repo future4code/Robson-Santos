005ce972c8231d3ffc7ede1481d8ca2e983406e1
robsonffsantos <robson.2501@hotmail.com>
2022-01-26 21:16:17 -0300

    exercicio 2

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,32 @@
+type produto = {
+    id: string,
+    name: string,
+    price: boolean
+}
+
+const arrayDeProduto: any = [
+    {
+        produtos: [
+            {
+                id: "25a2",
+                name: "laranja",
+                price: 3
+            },
+            {
+                id: "38b3",
+                name: "ameixa",
+                price: 2
+            },
+            {
+                id: "14c6",
+                name: "morango",
+                price: 4
+            },
+            {
+                id: "64d2",
+                name: "pessego",
+                price: 5
+            }
+        ]     
+    }
+]
