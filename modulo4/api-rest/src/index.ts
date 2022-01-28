1fe454c6f8a43da4664e8480e38849300a1c557c
robsonffsantos <robson.2501@hotmail.com>
2022-01-27 21:20:24 -0300

    exercicio 2

--- modulo4/api-rest/src/index.ts
+++ modulo4/api-rest/src/index.ts
@@ -17,6 +17,29 @@ app.get("/users", (req: Request, res: Response) => {
     }
   })
 
+//ex 2
+app.get("/user/tipo/:type", (req: Request, res: Response) => {
+    let errorCode = 400;
+  
+    try {
+      const result = users.filter(
+        (user: user) => user.type === req.params.type.toUpperCase()
+      )
+  
+      if (result.length === 0 || !result) {
+        errorCode = 404;
+        throw new Error("Nenhum dado foi achado, insira um type válido")
+      }
+  
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(errorCode)
+    }
+  })
+
+// a) path params
+// b) com filtro para que só os types válidos sejam enviados
+
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
