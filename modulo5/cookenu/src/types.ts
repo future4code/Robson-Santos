82fc1a6fe620a4e490edc3785ced47757a084cbb
robsonffsantos <robson.2501@hotmail.com>
2022-03-13 22:10:38 -0300

    types riados

--- modulo5/cookenu/src/types.ts
+++ modulo5/cookenu/src/types.ts
@@ -1,3 +1,19 @@
 export type authenticatorData = {
   id: string
 }
+
+export type user = {
+  id: string,
+  name: string,
+  email: string,
+  password: string
+}
+
+export type recipe = {
+  id: string,
+  title: string,
+  description: string,
+  createdAt: string,
+  authorId: string
+}
+
